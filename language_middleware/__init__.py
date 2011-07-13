6d31df372f8fb533f632a093691db58f8c24af97
Daniel Pérez Rada <dperezrada@gmail.com>
2011-07-13 12:40:24 -0400

    Now receiving configuration for default_language and valid_languages. Adding also new tests.

--- language_middleware/__init__.py
+++ language_middleware/__init__.py
@@ -1,26 +1,26 @@
 import re
 
-VALID_LANGUAGES = ('en', 'es')
-DEFAULT_LANGUAGE = 'en'
-
 class LanguageMiddleware(object):
-    def __init__(self, app):
+    def __init__(self, app, default_language = 'en', valid_languages = ('en', 'es')):
+        if default_language not in valid_languages:
+            raise Exception("Default language must be contained in valid languages")
         self.app = app
+        self.default_language = default_language
+        self.valid_languages = valid_languages
     
-    @staticmethod
-    def _validate_language(language):
-        if language and language in VALID_LANGUAGES:
+    def _validate_language(self, language):
+        if language and language in self.valid_languages:
             return language
     
     def _get_url_language(self, environ):
-        could_have_language = environ['PATH_INFO'][0:4]
+        could_have_language = environ.get('PATH_INFO', '')[0:4]
         language = re.findall('\/([a-z]{2,2})\/', could_have_language)
         if len(language) > 0:
             environ['PATH_INFO'] = environ['PATH_INFO'][3:] #Remove language from url
             return self._validate_language(language[0])
     
     def _get_headers_language(self, environ):
-        accept_language = environ.get("HTTP_ACCEPT_LANGUAGE")
+        accept_language = environ.get("HTTP_ACCEPT_LANGUAGE", "")
         if accept_language:
             accept_language = accept_language[0:2]
             if self._validate_language(accept_language):
@@ -35,5 +35,5 @@ class LanguageMiddleware(object):
             if headers_language and self._validate_language(headers_language):
                 environ['ACTIVE_LANGUAGE'] = headers_language
             else:
-                environ['ACTIVE_LANGUAGE'] = DEFAULT_LANGUAGE
+                environ['ACTIVE_LANGUAGE'] = self.default_language
         return self.app(environ, start_response)
--- tests/test_language_middleware.py
+++ tests/test_language_middleware.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
+'''
 test_language_middleware.py
-"""
+'''
 
 import unittest
 
@@ -10,30 +10,40 @@ from ludibrio import Stub, any
 
 from language_middleware import LanguageMiddleware
 
-class test_language_middleware(unittest.TestCase):
+class TestLanguageMiddleware(unittest.TestCase):
 	def setUp(self):
 	    with Stub() as app:
-	        app(any(), any()) >> "Some text ;)"
+	        app(any(), any()) >> 'Some text ;)'
 	    self.app = app
 	
 	def test_get_language_from_url_with_language(self):
 	    language_middleware = LanguageMiddleware(self.app)
 	    environ = {'PATH_INFO': '/es/ok'}
 	    language_middleware.__call__(environ, None)
-	    self.assertEquals("es", environ['ACTIVE_LANGUAGE'])
-	    self.assertEquals("/ok", environ['PATH_INFO'])
+	    self.assertEquals('es', environ['ACTIVE_LANGUAGE'])
+	    self.assertEquals('/ok', environ['PATH_INFO'])
 
 	def test_get_language_from_url_without_language(self):
 	    language_middleware = LanguageMiddleware(self.app)
 	    environ = {'PATH_INFO': '/documents/1'}
 	    language_middleware.__call__(environ, None)
-	    self.assertEquals("en", environ['ACTIVE_LANGUAGE'])
+	    self.assertEquals('en', environ['ACTIVE_LANGUAGE'])
+	    self.assertEquals('/documents/1', environ['PATH_INFO'])
 
 	def test_get_language_from_headers(self):
 	    language_middleware = LanguageMiddleware(self.app)
 	    environ = {'PATH_INFO': '/documents/1', 'HTTP_ACCEPT_LANGUAGE': 'es-cl,es;q=0.5'}
 	    language_middleware.__call__(environ, None)
-	    self.assertEquals("es", environ['ACTIVE_LANGUAGE'])
+	    self.assertEquals('es', environ['ACTIVE_LANGUAGE'])
+	
+	def test_default_language_should_be_in_valid_languages(self):
+	    self.assertRaises(Exception, LanguageMiddleware, self.app, 'fr')
+	
+	def test_get_language_using_another_default_language(self):
+	    language_middleware = LanguageMiddleware(self.app, default_language = 'fr', valid_languages = ('en', 'es', 'fr'))
+	    environ = {}
+	    language_middleware.__call__(environ, None)
+	    self.assertEquals('fr', environ['ACTIVE_LANGUAGE'])
     
 if __name__ == '__main__':
 	unittest.main()
