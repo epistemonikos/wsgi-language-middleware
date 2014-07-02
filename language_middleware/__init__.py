9d730277766a517941513037f7e04186640118e3
Felipe Valverde <felipe.valverde.campos@gmail.com>
2014-07-02 11:31:17 -0400

    fix: pep8

--- language_middleware/__init__.py
+++ language_middleware/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import gettext
 import os
 import re
@@ -5,27 +6,27 @@ import re
 
 class LanguageMiddleware(object):
     def __init__(
-            self,
-            app,
-            default_language = 'en',
-            valid_languages = ('en', 'es'),
-            clean_url = True,
-            locale_path = None,
-            locale_name = None
-        ):
+        self,
+        app,
+        default_language='en',
+        valid_languages=('en', 'es'),
+        clean_url=True,
+        locale_path=None,
+        locale_name=None
+    ):
         if default_language not in valid_languages:
             raise Exception("Default language must be contained in valid languages")
-        self.app = app
+
         self.default_language = default_language
         self.valid_languages = valid_languages
         self.clean_url = clean_url
         self.locale_path = locale_path
         self.locale_name = locale_name
-    
+
     def _validate_language(self, language):
         if language and language in self.valid_languages:
             return language
-    
+
     def _get_url_language(self, environ):
         could_have_language = environ.get('PATH_INFO', '')[0:4]
         if len(could_have_language) == 3:
@@ -33,16 +34,16 @@ class LanguageMiddleware(object):
         language = re.findall('\/([a-z]{2,2})\/', could_have_language)
         if len(language) > 0:
             if self.clean_url:
-                environ['PATH_INFO'] = environ['PATH_INFO'][3:] #Remove language from url
+                environ['PATH_INFO'] = environ['PATH_INFO'][3:]  # Remove language from url
             return self._validate_language(language[0])
-    
+
     def _get_headers_language(self, environ):
         accept_language = environ.get("HTTP_ACCEPT_LANGUAGE", "")
         if accept_language:
             accept_language = accept_language[0:2]
             if self._validate_language(accept_language):
                 return accept_language
-    
+
     def __call__(self, environ, start_response):
         url_language = self._get_url_language(environ)
         if url_language and self._validate_language(url_language):
