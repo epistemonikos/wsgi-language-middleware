df26018f433c2cc9987332e610566e56585dd3a0
Daniel Pérez Rada <dperezrada@gmail.com>
2016-02-01 22:57:21 -0300

    Moving Readme to Readme.md

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import sys, os
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-version = '0.5'
+version = '0.6'
 
 setup(
       name='language_middleware',
       version=version,
       description="This Wsgi middleware, takes the language from urls similiar to '/es/documents' or from the Accept language header.",
-      long_description=read('README'),
+      long_description=read('README.md'),
       classifiers=[
             "Development Status :: 4 - Beta",
             "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
@@ -19,7 +19,7 @@ setup(
       ],
       keywords='wsgi middleware language i18n translation',
       author='Daniel Perez Rada',
-      author_email='daniel at etiqs com',
+      author_email='dperezrada at gmail',
       url='https://github.com/dperezrada/wsgi-language-middleware',
       license='GNU Public License v3.0',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
